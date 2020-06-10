9b6be1b6866b83db8bd3e2fadd3232e1692e4299
sota ibaraki <souta1818@gmail.com>
2020-06-10 23:14:55 +0900

    ユーザーIDの取得、fetch_text fix

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -40,12 +40,12 @@ def mention_func(message): # argsはメッセージの内容を取ってる mess
     # 1. 型を確認する → type(変数)
     # 2. 型に応じて分解する
 
-    result = fetch_text() # 過去の投稿データ
+    result = fetch_text('CC9NTUBEY') # 過去の投稿データ
     # print(result)
     msg = result['messages']
-    print(len(msg))
-    music_link = msg[0]['attachments'][0]['title_link']
- 
+    target_user = msg[0]['user']
+    # print(len(msg))
+    # music_link = msg[0]['attachments'][0]['title_link']
 
     # result["text"] = result.get("text") →　中身が出てくる
     music_list = [] 
@@ -54,12 +54,15 @@ def mention_func(message): # argsはメッセージの内容を取ってる mess
             if n['attachments'][0].get('title_link') is not None:
                  music_list.append(n['attachments'][0]['title_link'])
 
+    # music_list.append({n['user'] , n['attachments'][0]['title_link']})
+    # [{user: XXX, url: YYY},{user: XXX, url: YYY},{user: XXX, url: YYY}]
+
     # @share_music_bot: 音楽
     # [URL1, URL2, URL3, URL4, URL5, URL6, .....] → ここからランダム
-    # print(music_list)
+    # print(target_user)
 
     randnum = random.randrange(0,len(music_list)) #len(music_list)で今ある投稿分の乱数を指定
-    music_list[randnum] #
+    music_list[randnum] # こんにちわ
 
     # msg = result['messages'] # リスト型
     # # print(msg,type(msg))
@@ -79,7 +82,7 @@ def mention_func(message): # argsはメッセージの内容を取ってる mess
     # {"UGES7CGJU": [URL1, URL2, URL3], "QWEZZRT2": [URL1], ....}
 
 
-    message.reply(f'これおすすめやで\n{music_list[randnum]}')   c
+    message.reply(f'これおすすめやで\n{music_list[randnum]}') 
     #message.reply(message.body['text']) # メンション
     # message.reply(result['text'])
     
@@ -90,20 +93,23 @@ def mention_func(message): # argsはメッセージの内容を取ってる mess
 #     message.send('誰かがリッスンと投稿したようだ')      # ただの投稿
 #     message.reply('君だね？')
 
-SLACK_CHANNEL_ID = "CC9NTUBEY"
+# SLACK_CHANNEL_ID = "CC9NTUBEY"
 SLACK_URL = "https://slack.com/api/conversations.history"
 TOKEN = os.environ["TOKEN"]
 # チャンネルの投稿を全て取得
-def fetch_text():
+def fetch_text(channel_id):
     """ share_musicチャンネルの投稿を取得してjsonに変換
     Returns:
         str: チャンネル投稿のデータjson
     """
     payload = {
-        "channel": SLACK_CHANNEL_ID,
+        "channel": channel_id,
         "token": TOKEN,
         "limit": 1000,
     }
     response = requests.get(SLACK_URL, params=payload)
     json_data = response.json()
     return json_data
+
+# def 関数(): で別API情報　実装予定
+
