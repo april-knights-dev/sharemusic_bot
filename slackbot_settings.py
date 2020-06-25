3744b9aac7d629e1f4075ca84bc0fa1203a19b7f
sota ibaraki <souta1818@gmail.com>
2020-06-25 23:13:09 +0900

    デプロイ

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -5,7 +5,7 @@ import os
 API_TOKEN = os.environ["API_TOKEN"]
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
-DEFAULT_REPLY = "musicまたはumameshiってメンションしてね"
+DEFAULT_REPLY = "'音楽'または'うまめし'ってメンションしてぽよ"
 
 # プラグインスクリプトを置いてあるサブディレクトリ名のリスト
 PLUGINS = ['plugins']
